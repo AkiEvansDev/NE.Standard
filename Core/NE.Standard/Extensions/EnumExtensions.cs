173e8a3ab0e7adf0b7e975d0c906e38acb3ff9b2
AkiEvansDev <36594449+AkiEvansDev@users.noreply.github.com>
2025-06-30 16:47:10 +0300

    Implement extension helpers

--- Core/NE.Standard/Extensions/EnumExtensions.cs
+++ Core/NE.Standard/Extensions/EnumExtensions.cs
@@ -1,10 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
 
 namespace NE.Standard.Extensions
 {
+    /// <summary>
+    /// Useful helpers for working with enums.
+    /// </summary>
     public static class EnumExtensions
     {
+        public static string GetName(this Enum @enum)
+            => Enum.GetName(@enum.GetType(), @enum)!;
+
+        public static string[] GetNames(this Enum @enum)
+            => Enum.GetNames(@enum.GetType());
+
+        public static IEnumerable<T> GetValues<T>(this Enum @enum) where T : Enum
+            => Enum.GetValues(@enum.GetType()).Cast<T>();
+
+        public static string GetDescription(this Enum @enum)
+            => @enum.GetAttribute<DescriptionAttribute>().Description;
+
+        public static IEnumerable<(T Value, string Description)> GetDescriptions<T>(this Enum @enum) where T : Enum
+        {
+            foreach (T value in @enum.GetValues<T>())
+                yield return (value, value.GetAttribute<DescriptionAttribute>().Description);
+        }
+
+        public static TAttribute GetAttribute<TAttribute>(this Enum @enum) where TAttribute : Attribute
+        {
+            return @enum
+                .GetType()
+                .GetMember(@enum.GetName()!)[0]
+                .GetCustomAttribute<TAttribute>();
+        }
     }
 }
--- Core/NE.Standard/Extensions/GeneralConstants.cs
+++ Core/NE.Standard/Extensions/GeneralConstants.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 
 namespace NE.Standard.Extensions
 {
+    /// <summary>
+    /// Common constants shared across extensions.
+    /// </summary>
     public static class GeneralConstants
     {
+        public const string DATETIME_FORMAT = "MM.dd.yyyy HH:mm:ss.f";
+        public const string TIMESPAN_FORMAT = "c";
+
+        public static readonly NumberFormatInfo NumberFormat = new()
+        {
+            NumberGroupSeparator = ".",
+            NumberDecimalSeparator = "."
+        };
     }
 }
